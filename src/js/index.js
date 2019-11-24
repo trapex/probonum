fdb3df38c525517299ba9d0733a0b672ac86e3c8
tatyana-ivanova <tatyana.ivanova@isobar.ru>
2019-11-24 23:22:07 +0300

    Add probonium markup and styles

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,14 @@ import style from "../css/style.scss";
 if (process.env.NODE_ENV === 'development') {
 	console.log('Working in development mode');
 }
+
+$(function() {
+	let $next = $('.js-next-btn'),
+		$platform = $('.platform');
+
+	$next.on('click', (e) => {
+		$('html, body').animate({
+			scrollTop: $platform.offset().top
+		}, 300);
+	});
+});
